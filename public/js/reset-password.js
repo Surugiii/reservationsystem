bf4aa3ace4c717603f011edba7929d8ee514772f
Surugiii <carlstzmejela@gmail.com>
2025-10-20 18:04:59 +0800

    Update reset-password.js

--- public/js/reset-password.js
+++ public/js/reset-password.js
@@ -31,7 +31,7 @@ form.addEventListener('submit', async (e) => {
 
   try {
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: https://surugiii.github.io/reservationsystem/public/change-password.html,
+      redirectTo: 'https://surugiii.github.io/reservationsystem/public/change-password.html',
       captchaToken: null
     });
 
@@ -91,4 +91,5 @@ function showStatus(message, type) {
       updateToggleState();
 
     });
+
 
